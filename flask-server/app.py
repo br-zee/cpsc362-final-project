d5d1eea7bec7c57e59a0a8cdfcfd46dc0ae6ebd0
bzee2004 <brianzee2004@gmail.com>
2025-04-07 01:11:35 -0700

    adwad

--- flask-server/app.py
+++ flask-server/app.py
@@ -1,14 +1,14 @@
 from flask import Flask
+from flask_cors import CORS
 
-app = Flask(__name__, static_folder="../client/build", static_url_path="/")
-
-@app.route("/")
-def index():
-    return app.send_static_file("index.html")
+app = Flask(__name__)
+CORS(app)
 
 @app.route("/api/members")
 def members():
     return {"members": ["Member 1", "Member 2", "Member 3"]}
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0")
+    import os
+    port = int(os.envion.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
