8b97d8a1226730b5dca7368400fa79840e5151a4
bzee2004 <brianzee2004@gmail.com>
2025-04-07 01:16:35 -0700

    edited typo

--- flask-server/app.py
+++ flask-server/app.py
@@ -10,5 +10,5 @@ def members():
 
 if __name__ == "__main__":
     import os
-    port = int(os.envion.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
