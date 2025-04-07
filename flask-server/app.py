05e77f56ba0fefbe0f9fe1c070cf1cc33f1dd727
bzee2004 <brianzee2004@gmail.com>
2025-04-07 00:54:27 -0700

    deployment changes

---
+++ flask-server/app.py
@@ -0,0 +1,14 @@
+from flask import Flask
+
+app = Flask(__name__, static_folder="../client/build", static_url_path="/")
+
+@app.route("/")
+def index():
+    return app.send_static_file("index.html")
+
+@app.route("/api/members")
+def members():
+    return {"members": ["Member 1", "Member 2", "Member 3"]}
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0")
